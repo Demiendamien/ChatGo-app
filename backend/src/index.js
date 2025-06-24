94ac7d0b87da047766128f10f8a92ec7ea9f2048
Demiendamien <demiendamien5@gmail.com>
2025-06-24 04:40:55 +0200

    correction: mise à jour de la configuration d'axios, les paramètres CORS et ajout d'un fichier .env au frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,11 @@ import cors from 'cors'
 
 import path from 'path';
 
-import authRoutes from '../routes/auth.route.js';
-import messageRoutes from '../routes/message.route.js';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 
-import { connectDB } from '../lib/db.js';
-import { app, server } from '../lib/socket.js';
+import { connectDB } from './lib/db.js';
+import { app, server } from './lib/socket.js';
 
 dotenv.config();
 
@@ -21,12 +21,14 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
-app.use(cors(
-    {
-        origin: ["http://localhost:5173", "http://localhost:5174"],
-        credentials: true,
-    }
-));
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://chatgo-app-front.onrender.com"
+    ],
+    credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
-import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL || "https://chat-app-2.onrender.com/api";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development"
-        ? "http://localhost:5101/api"
-        : "https://chat-app-2.onrender.com/api",
-    withCredentials: true,
+  baseURL: API_URL,
+  withCredentials: true,
 });
