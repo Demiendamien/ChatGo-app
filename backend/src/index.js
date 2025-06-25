4f4d732a4f0ed9c099c5c36c98a63e16f2cae724
Demiendamien <demiendamien5@gmail.com>
2025-06-25 16:50:44 +0200

    fix cors gesteur 02

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,7 +40,7 @@ const corsOptions = {
 
 // Place CORS AVANT toutes les routes
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+//app.options("*", cors(corsOptions));
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
