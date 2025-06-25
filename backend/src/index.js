fef9f667abcb199e145d2ab259c3138c008a41d3
Demiendamien <demiendamien5@gmail.com>
2025-06-25 16:13:34 +0200

    fix cors10

--- backend/src/index.js
+++ backend/src/index.js
@@ -53,9 +53,10 @@ app.get("/cors-test", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
+  app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+
 }
 
 server.listen(PORT, () => {
